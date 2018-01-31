a4ad6f8eae09c3eb004b3f0776b20ab00194b42b
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-31 15:34:48 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    static String[] masyvas;
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        masyvas = new String[5];
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println("Iveskite zodi");
+            String zodis = sc.nextLine();
+            masyvas[i] = zodis;
+        }
+        atvaizdavimas();
+    }
+    public static void atvaizdavimas() {
+        System.out.println("visi zodziai ");
+        for (int i = 0; i <masyvas.length; i++) {
+            System.out.print(masyvas[i]+ " ");
+
+        }
     }
 }
